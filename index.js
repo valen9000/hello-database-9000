73a9afa3e3a226a4fd12362c8ab7aedbed629199
franaguee <67479979+franaguee@users.noreply.github.com>
2020-08-27 21:29:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const router = require('./routes/api/user');
 const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
 
+const cors = require('cors');
+
+
 const app = express();
+
+app.use(cors());
+
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
 mongoose
@@ -12,8 +18,8 @@ mongoose
   .then(() => {
     console.log(`DB connected @ ${db}`);
   })
-  .catch(err => console.error(`Connection error ${err}`));
-  app.use('/api', router);
+.catch(err => console.error(`Connection error ${err}`));
+app.use('/api', router);
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
